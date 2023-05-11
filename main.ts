7c8e7dd2fe33f039a28370a2190e4168df81a7a7
fnavizq355 <132046114+fnavizq355@users.noreply.github.com>
2023-05-11 14:36:07 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,26 +1,46 @@
+function accion_A () {
+    maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, velocidad)
+    basic.pause(500)
+    parada = 0
+    anda = true
+}
+function en_parada () {
+    if (parada < cantidad) {
+        radio.sendString("" + (arboles[parada]))
+        parada = parada + 1
+        basic.pause(2000)
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, velocidad)
+        basic.pause(1000)
+    } else {
+        radio.sendString("Fin del paseo")
+        anda = false
+    }
+}
+function accion_B () {
+    if (anda) {
+        anda = false
+        maqueen.motorStop(maqueen.Motors.All)
+        radio.sendString("Paseo detenido")
+    } else {
+        anda = true
+        radio.sendString("Paseo reanudado")
+    }
+}
 radio.onReceivedString(function (receivedString) {
     if (receivedString == "A") {
-        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, velocidad)
-        basic.pause(500)
-        parada = 0
-        anda = true
+        accion_A()
     }
     if (receivedString == "B") {
-        if (anda) {
-            anda = false
-            maqueen.motorStop(maqueen.Motors.All)
-            radio.sendString("Paseo detenido")
-        } else {
-            anda = true
-            radio.sendString("Paseo reanudado")
-        }
+        accion_B()
     }
 })
 let parada = 0
+let cantidad = 0
 let anda = false
 let velocidad = 0
+let arboles: string[] = []
 radio.setGroup(1)
-let arboles = [
+arboles = [
 "Palmera",
 "Naranjo",
 "Jacaranda",
@@ -30,7 +50,7 @@ let arboles = [
 ]
 velocidad = 50
 anda = false
-let cantidad = arboles.length
+cantidad = arboles.length
 basic.forever(function () {
     if (anda) {
         if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 0 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 0) {
@@ -41,16 +61,7 @@ basic.forever(function () {
             maqueen.motorStop(maqueen.Motors.M1)
         } else {
             maqueen.motorStop(maqueen.Motors.All)
-            if (parada < cantidad) {
-                radio.sendString("" + (arboles[parada]))
-                parada = parada + 1
-                basic.pause(2000)
-                maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, velocidad)
-                basic.pause(1000)
-            } else {
-                radio.sendString("Fin del paseo")
-                anda = false
-            }
+            en_parada()
         }
     }
 })
